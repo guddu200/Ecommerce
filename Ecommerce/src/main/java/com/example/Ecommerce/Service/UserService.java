99648154f7e6928de059d0a3ca4686f332f0978f
guddu200 <kumar.guddu@jda.com>
2024-02-05 10:22:56 -0600

    Updated UserService

--- Ecommerce/src/main/java/com/example/Ecommerce/Service/UserService.java
+++ Ecommerce/src/main/java/com/example/Ecommerce/Service/UserService.java
@@ -18,6 +18,7 @@ public class UserService {
 
     public Optional<User> updateuser(User user){
         Optional<User>updateuser=userRepo.saveAndFlush(user);
+        System.out.println("Hello World");
         return updateuser;
     }
 
